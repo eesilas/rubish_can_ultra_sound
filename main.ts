4e0bab861654539f7586227fa29c7eb8fa21df66
Learn STEM by Doing <32452061+eesilas@users.noreply.github.com>
2025-02-07 15:03:52 +0800

    12 trig 16 echo

--- main.ts
+++ main.ts
@@ -1,15 +1,21 @@
-basic.showIcon(IconNames.Sword)
+basic.showIcon(IconNames.SmallHeart)
 let distance = 0
+microIoT.microIoT_ServoRun(microIoT.aServos.S1, 0)
+basic.pause(2000)
 basic.forever(function () {
-    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.Yes)
     distance = sonar.ping(
     DigitalPin.P12,
-    DigitalPin.P14,
-    PingUnit.MicroSeconds
+    DigitalPin.P16,
+    PingUnit.Centimeters
     )
     if (distance < 15) {
         microIoT.microIoT_ServoRun(microIoT.aServos.S1, 90)
         microIoT.microIoT_setIndexColor(PIN.P15, 0, 2, 0x00ff00)
+        basic.pause(5000)
+        microIoT.microIoT_ServoRun(microIoT.aServos.S1, 0)
+        microIoT.microIoT_setIndexColor(PIN.P15, 0, 2, 0x7f00ff)
+        basic.pause(5000)
     } else {
         microIoT.microIoT_ServoRun(microIoT.aServos.S1, 0)
         microIoT.microIoT_setIndexColor(PIN.P15, 0, 2, 0xff0000)
