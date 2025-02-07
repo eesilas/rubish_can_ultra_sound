75e3b5fbc79bb335c4cc16397bc98ccc917b6440
Learn STEM by Doing <32452061+eesilas@users.noreply.github.com>
2025-02-07 15:19:22 +0800

    Version class teach.

--- main.ts
+++ main.ts
@@ -13,11 +13,11 @@ basic.forever(function () {
         microIoT.microIoT_ServoRun(microIoT.aServos.S1, 90)
         microIoT.microIoT_setIndexColor(PIN.P15, 0, 2, 0x00ff00)
         basic.pause(5000)
-        microIoT.microIoT_ServoRun(microIoT.aServos.S1, 0)
+        microIoT.microIoT_ServoRun(microIoT.aServos.S1, 9)
         microIoT.microIoT_setIndexColor(PIN.P15, 0, 2, 0x7f00ff)
         basic.pause(5000)
     } else {
-        microIoT.microIoT_ServoRun(microIoT.aServos.S1, 0)
+        microIoT.microIoT_ServoRun(microIoT.aServos.S1, 9)
         microIoT.microIoT_setIndexColor(PIN.P15, 0, 2, 0xff0000)
     }
 })
